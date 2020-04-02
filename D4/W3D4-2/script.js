ccfee9b80fb0cd3253c67ead7bd565994ac4ebf4
Lars Roovers <larsroovers@gmail.com>
2020-04-02 18:09:48 +0200

    fixed some bugs

--- D4/W3D4-2/script.js
+++ D4/W3D4-2/script.js
@@ -2,7 +2,12 @@ const button = document.getElementById('action-button');
 const container = document.getElementById('content-container');
 const input = document.getElementById('text-input');
 
+const clearContainer = () => {
+    container.innerHTML = "";
+}
+
 const quotesToDom = (data) => {
+    clearContainer();
     data.map(item => {
         const quote = document.createElement('blockquote');
         const textnode = document.createTextNode(item.value);
@@ -13,27 +18,41 @@ const quotesToDom = (data) => {
 }
 
 const handleChangeEvent = async (event) => {
-    const res = await getSearchQuery(event.target.value);
-    const quotes = res._embedded.quotes;
-    quotesToDom(quotes);
+    try {
+        const res = await getSearchQuery(event.target.value);
+        const quotes = res._embedded.quotes;
+        quotesToDom(quotes);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 const getSearchQuery = async (query) => {
-    const res = await fetch(`https://tronalddump.io/search/quote?query=${query}`, {method: 'GET'});
-    const json = await res.json();
-    console.log(json);
-    return json;
+    try {
+        const res = await fetch(`https://tronalddump.io/search/quote?query=${query}`, {method: 'GET'});
+        const json = await res.json();
+        console.log(json);
+        return json;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 
 const getRandomQuote = async () => {
-    const res = await fetch('https://tronalddump.io/random/quote', {method: 'GET'});
-    const json = await res.json();
-    const quote = document.createElement('blockquote');
-    const textnode = document.createTextNode(json.value);
-    quote.appendChild(textnode);
-    quote.classList.add('quote');
-    container.append(quote);
+    try{
+        const res = await fetch('https://tronalddump.io/random/quote', {method: 'GET'});
+        const json = await res.json();
+        console.log(json);
+        const quote = document.createElement('blockquote');
+        const textnode = document.createTextNode(json.value);
+        quote.appendChild(textnode);
+        quote.classList.add('quote');
+        container.append(quote);
+    } catch(err) {
+        console.log(err);
+    }
+    
 }
 
 button.addEventListener('click', getRandomQuote);
